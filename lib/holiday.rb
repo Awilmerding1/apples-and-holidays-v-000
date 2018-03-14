4e808c522fd4b2050e40b7c49c96d7894a9090b7
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 14:56:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,14 +53,11 @@ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-array = []
+
   holiday_supplies.collect do |seasons, data|
     data.collect do |holiday, list|
      if list.include?("BBQ")
-       array << holiday
-       array.collect do |bbq|
-         bbq
-  end
+       holiday
  end
  end
 end
