793e132f9854cc632b570566cdca6133b0dbf5f1
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-14 15:06:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,10 +23,11 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  string = ""
  holiday_supplies[:winter].collect do |holiday, list|
-   list.join(",")
+  string = list.join(",")
  end
- 
+ string.split(",")
 end
 
 def all_supplies_in_holidays(holiday_hash)
